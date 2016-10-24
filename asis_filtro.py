7c973f3f277f4e2a8b6794498c4c35401b6d4817
Juan Blanc <juanblanc14@gmail.com>
2016-10-24 19:36:32 -0300

    Add files via upload

---
+++ asis_filtro.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+from bs4 import BeautifulSoup
+
+def asistencia():
+    respuesta = urllib.urlopen('http://www.frp.utn.edu.ar/asis_doc/index.php')
+    html = respuesta.read()
+
+    sopa = BeautifulSoup(html,'html.parser')
+
+    puro = sopa.get_text()
+    filtrado = puro.split('2016')
+    filtrado = filtrado[1].split('Registro')
+    filtrado = filtrado[0].split('\n')
+     
+    return filtrado
